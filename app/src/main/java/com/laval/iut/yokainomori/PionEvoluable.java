d1769485aa0f783ffa7c8f858d5b0204f23ec8f2
Riwal <riwal.lefaou7@gmail.com>
2016-12-11 16:33:12 +0100

    Règle kodama non testée

---
+++ app/src/main/java/com/laval/iut/yokainomori/PionEvoluable.java
@@ -0,0 +1,59 @@
+package com.laval.iut.yokainomori;
+
+import java.util.List;
+
+/**
+ * Created by Nicolas on 28/09/2016.
+ */
+
+public class PionEvoluable extends Pion implements Evoluable {
+	
+	private Pion evolution;
+	private boolean isEvolue;
+
+	public PionEvoluable(String nom, String img, List<Deplacement> deplacements, Pion evolution) {
+		super(nom, img, deplacements);
+		this.evolution = evolution;
+		this.isEvolue = false;
+	}
+
+	@Override
+	public void seRetourner() {
+		evolution.seRetourner();
+		super.seRetourner();
+	}
+
+	@Override
+	public String getNom() {
+		if (isEvolue)
+			return evolution.getNom();
+		else return super.getNom();
+		
+	}
+
+	@Override
+	public List<Deplacement> getDeplacements() {
+		if(!isEvolue)
+			return super.getDeplacements();
+		else return evolution.getDeplacements();
+	}
+
+	@Override
+	public String getImg() {
+		if (!isEvolue)
+			return super.getImg();
+		else return evolution.getImg();
+	}
+
+	public void evoluer(){
+    	isEvolue = true;
+    }
+    public void desevoluer(){
+    	isEvolue = false;
+    }
+
+
+	public boolean isEvolue() {
+		return isEvolue;
+	}
+}
